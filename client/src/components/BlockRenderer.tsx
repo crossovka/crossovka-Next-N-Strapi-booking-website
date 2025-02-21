92caab0abf958789bce2a9a7d1f5bc39fdf66216
crossovka <best-login-mail@yandex.ru>
2025-02-21 20:25:20 +0900

    add newsletter sign up

--- client/src/components/BlockRenderer.tsx
+++ client/src/components/BlockRenderer.tsx
@@ -1,28 +1,35 @@
-import type { Block, FeaturedArticleProps, HeroSectionProps, InfoBlockProps, SubscribeProps } from '@/types';
+import type {
+	Block,
+	FeaturedArticleProps,
+	HeroSectionProps,
+	InfoBlockProps,
+	SubscribeProps,
+} from '@/types';
 import { HeroSection } from '@/components/blocks/HeroSection';
 import { InfoBlock } from '@/components/blocks/InfoBlock';
 import { FeaturedArticle } from './blocks/FeaturedArticle';
 import { Subscribe } from './blocks/Subscribe';
 
-// Рендерим блоки в зависимости от их типа (по типу компонента)
 function blockRenderer(block: Block, index: number) {
+	const uniqueKey = `${block.__component}-${block.id || index}`; // Безопасный ключ
+
 	switch (block.__component) {
 		case 'blocks.hero-section':
-			return <HeroSection {...(block as HeroSectionProps)} key={block.id} />;
+			return <HeroSection {...(block as HeroSectionProps)} key={uniqueKey} />;
 		case 'blocks.info-block':
-			return <InfoBlock {...(block as InfoBlockProps)} key={block.id} />;
+			return <InfoBlock {...(block as InfoBlockProps)} key={uniqueKey} />;
 		case 'blocks.featured-article':
 			return (
-				<FeaturedArticle {...(block as FeaturedArticleProps)} key={block.id} />
+				<FeaturedArticle {...(block as FeaturedArticleProps)} key={uniqueKey} />
 			);
 		case 'blocks.subscribe':
-			return <Subscribe {...(block as SubscribeProps)} key={block.id} />;
+			return <Subscribe {...(block as SubscribeProps)} key={uniqueKey} />;
 		default:
 			console.warn(`Неизвестный блок: ${block.__component}`);
-			return null; // Если тип блока не поддерживается, возвращаем null
+			return null;
 	}
 }
 
 export function BlockRenderer({ blocks }: { blocks: Block[] }) {
-	return <>{blocks.map(blockRenderer)}</>; // Рендерим все блоки
+	return <>{blocks.map(blockRenderer)}</>;
 }
